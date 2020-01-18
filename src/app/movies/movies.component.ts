058483273e0b664caf93ab35ff61f488c9d56f9e
paulayo93 <52790286+paulayo93@users.noreply.github.com>
2020-01-18 21:31:47 +0100

    fix void type error

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -79,7 +79,7 @@ export class MoviesComponent implements OnInit {
     return this.favoriteService.hasVoted(movie);
   }
 
-   getPaginatorData(event){
+   getPaginatorData(event?: pageEvent){
      console.log(event);
      if(event.pageIndex === this.pageIndex + 1){
         this.lowValue = this.lowValue + this.pageSize;
